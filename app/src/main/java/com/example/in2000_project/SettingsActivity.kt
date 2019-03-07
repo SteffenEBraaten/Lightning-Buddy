7b906c67b6434b940e121a9a9b2ed5517a606364
tmcheung <tmcheung@student.matnat.uio.no>
2019-03-07 14:00:24 +0100

    Added preference resource file

---
+++ app/src/main/java/com/example/in2000_project/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.in2000_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
