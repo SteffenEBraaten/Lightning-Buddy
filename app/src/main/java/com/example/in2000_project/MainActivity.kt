0e9185547980eafb9568e8b008fce3b5b47c4e9d
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-03-07 12:48:25 +0100

    Implement google maps fragment in main activity

--- app/src/main/java/com/example/in2000_project/MainActivity.kt
+++ app/src/main/java/com/example/in2000_project/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.in2000_project
 
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         super.setToolbar(getString(R.string.app_name), false)
+
+        //Get notified when map is ready to be used
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+    }
+
 }
