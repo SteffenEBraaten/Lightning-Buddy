ae162b44ddf31b699728a8ce0078c373c1a2a882
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-03-15 12:04:05 +0100

    Add search bar fragment. Move API key to string resource for ease of use in multiple files

--- app/src/main/java/com/example/in2000_project/MainActivity.kt
+++ app/src/main/java/com/example/in2000_project/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.in2000_project
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -13,8 +16,14 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
-class MainActivity : BaseActivity(), OnMapReadyCallback {
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+class MainActivity : BaseActivity(), OnMapReadyCallback, PlaceSelectionListener {
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
@@ -31,6 +40,10 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        Places.initialize(applicationContext, getString(R.string.Maps_API))
+        //TODO: Mulig å slette?
+        val placesClient: PlacesClient = Places.createClient(this)
     }
 
     val MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 100
@@ -48,7 +61,6 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
                 , MY_PERMISSIONS_REQUEST_ACCESS_LOCATION)
         }
 
-
     }
     private fun setUpMap() {
         // The reason for checking this all the time is because the user could at any time revoke permissions
@@ -62,6 +74,20 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
                 }
             }
         }
+        val autocompleteFragment: AutocompleteSupportFragment =
+            supportFragmentManager.findFragmentById(R.id.searchbar_fragment) as AutocompleteSupportFragment
+
+        autocompleteFragment.setPlaceFields(arrayListOf(Place.Field.ID, Place.Field.NAME))
+
+        autocompleteFragment.setOnPlaceSelectedListener(this)
+    }
+
+    override fun onPlaceSelected(p0: Place) {
+
+    }
+
+    override fun onError(status: Status) {
+        Toast.makeText(applicationContext, ""+status.toString(), Toast.LENGTH_LONG).show()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -81,5 +107,9 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
             //Ignore all other requests
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
