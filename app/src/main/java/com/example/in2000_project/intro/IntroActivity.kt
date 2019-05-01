dcd7cf8f2793de2699bd73c8e87923f61bdf093d
steffeeb <steffeeb@uio.no>
2019-05-01 18:25:17 +0200

    Added functional first time user slider

---
+++ app/src/main/java/com/example/in2000_project/intro/IntroActivity.kt
@@ -0,0 +1,134 @@
+package com.example.in2000_project.intro
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import com.example.in2000_project.R
+import com.example.in2000_project.maps.MainActivity
+import kotlinx.android.synthetic.main.activity_intro.*
+
+class IntroActivity : AppCompatActivity() {
+
+    private val slider1 = SliderFragment()
+    private val slider2 = SliderFragment()
+    private val slider3 = SliderFragment()
+
+    lateinit var prefrence : SharedPreferences
+    private val intro_flag = "Intro"
+
+    lateinit var adapter: MyPageAdapter
+    lateinit var activity: Activity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        prefrence = getSharedPreferences("FirstTimeIntro", Context.MODE_PRIVATE)
+        activity = this
+
+        if(!prefrence.getBoolean(intro_flag, true)){
+            startActivity(Intent(activity, MainActivity::class.java))
+            finish()
+        }
+
+        slider1.setTitle("Memes")
+
+
+        slider2.setTitle("are")
+
+
+        slider3.setTitle("good")
+
+
+        adapter = MyPageAdapter(supportFragmentManager)
+
+        adapter.sliderList.add(slider1)
+        adapter.sliderList.add(slider2)
+        adapter.sliderList.add(slider3)
+
+        intro_viewPager.adapter = adapter
+
+        // Initializing btn_next
+        intro_btn_next.setOnClickListener {
+            intro_viewPager.currentItem++
+        }
+
+        // Initializing btn_skip
+        intro_btn_skip.setOnClickListener {
+            goToMainActivity()
+        }
+        intro_viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(p0: Int) {
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+            }
+
+            override fun onPageSelected(p0: Int) {
+                // We're at the last page
+                if (p0 == adapter.sliderList.size - 1) {
+                    intro_btn_next.text = getText(R.string.done)
+                    intro_btn_next.setOnClickListener{
+                        goToMainActivity()
+                    }
+                }
+                else {
+                    // There is a next
+                    //intro_btn_next.text = getText(R.string.next)
+                    intro_btn_next.setOnClickListener {
+                        intro_viewPager.currentItem++
+                    }
+                }
+                // Change the progressbar color
+                when(intro_viewPager.currentItem) {
+                    0 ->{
+                        intro_progress_1.setTextColor(Color.BLACK)
+                        intro_progress_2.setTextColor(Color.GRAY)
+                        intro_progress_3.setTextColor(Color.GRAY)
+                    }
+                    1 -> {
+                        intro_progress_1.setTextColor(Color.GRAY)
+                        intro_progress_2.setTextColor(Color.BLACK)
+                        intro_progress_3.setTextColor(Color.GRAY)
+                    }
+                    2 -> {
+                        intro_progress_1.setTextColor(Color.GRAY)
+                        intro_progress_2.setTextColor(Color.GRAY)
+                        intro_progress_3.setTextColor(Color.BLACK)
+                    }
+                }
+            }
+        })
+    }
+
+    // Takes user to MainActivity and sets flag to false
+    private fun goToMainActivity(){
+        startActivity(Intent(activity, MainActivity::class.java))
+        finish()
+        val editor = prefrence.edit()
+        editor.putBoolean(intro_flag, false)
+        editor.apply()
+    }
+
+    class MyPageAdapter(manager : FragmentManager) : FragmentPagerAdapter(manager) {
+
+        val sliderList : MutableList<Fragment> = ArrayList()
+
+        override fun getItem(p0: Int): Fragment {
+            return sliderList[p0]
+        }
+
+        override fun getCount(): Int {
+            return sliderList.size
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/in2000_project/intro/SliderFragment.kt
@@ -0,0 +1,46 @@
+package com.example.in2000_project.intro
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_slider.*
+
+import com.example.in2000_project.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SliderFragment : Fragment() {
+
+    var tempTitle : String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slider, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        slider_title.text = tempTitle
+    }
+
+    //Sets the title of the sliderFragment
+    fun setTitle(title : String){
+        tempTitle = title
+    }
+
+    //Sets the picture of the
+    //fun setPicture(path : String){}
+
+}
